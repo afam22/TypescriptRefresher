98ae5d9da44535e4319d4b851f2e372575fe9b8b
afam22 <happyguy_afam@yahoo.com>
2024-08-25 21:16:04 -0600

    error for adding type in the function

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
-function add(n1, n2){
+function add(n1: number, n2: number){
     return n1 + n2;
 }
 
 const number1 = '5';
 const number2 = 2.8;
 
-const result = add(number1,number2); //52.8 is the result
+const result = add(number1,number2); //throws a compilation error - benefit of typescript because number 1 is a string and wont compile
 
 console.log(result);
