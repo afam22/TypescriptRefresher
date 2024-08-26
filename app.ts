045af6b2cc90a5cbef0069af6db192f754a8da5a
afam22 <happyguy_afam@yahoo.com>
2024-08-25 21:22:02 -0600

    In JS, you can use this to check for compile error

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
 function add(n1: number, n2: number){
-    return n1 + n2;
+    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
+        throw new Error ('incorrect input type');
+    }
+    return result;
 }
 
 const number1 = '5';
 const number2 = 2.8;
 
-const result = add(number1,number2); //throws a compilation error - benefit of typescript because number 1 is a string and wont compile
+const result = add(number1,number2); 
 
 console.log(result);
