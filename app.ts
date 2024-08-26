5f44e664415a366db7dbb7f22c201b5627dcd30b
afam22 <happyguy_afam@yahoo.com>
2024-08-25 21:29:46 -0600

    introducing string and boolean data types

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
-function add(n1: number, n2: number){
-    if(typeof n1 !== 'number' || typeof n2 !== 'number'){
-        throw new Error ('incorrect input type');
-    }
+function add(n1: number, n2: number, showResult: boolean, phrase: string){
+   const result = n1 + n2 // this first calculates the addition before it is concatenated with a string phrase
+
+   if(showResult){
+    console.log(phrase + result);
+   }else {
     return result;
+   }
 }
 
-const number1 = '5';
+const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
-const result = add(number1,number2); 
-
-console.log(result);
+const result = add(number1,number2, printResult, resultPhrase); 
